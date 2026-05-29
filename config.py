fb21fbfd1022c53b865d47f5d3cadd5f6b14bd5b
ish1n <ishangupta409@gmail.com>
2026-05-29 16:08:09 +0530

    Update WhatsApp Meta account credentials

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 VERIFY_TOKEN = "verify_token_123"
-PHONE_NUMBER_ID = "969861266221206"
+PHONE_NUMBER_ID = "1109427478922656"
 
 # This pulls the token safely from Render's secure vault later!
 WHATSAPP_TOKEN = os.environ.get("WHATSAPP_TOKEN")
